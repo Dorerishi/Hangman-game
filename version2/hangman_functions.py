0125181a08ce96ac2d2b90597a4fdae072183b23
Dorerishi <hrushikeshraghvendra@gmail.com>
2024-02-17 20:08:00 +0530

    adding start screen

--- version2/hangman_functions.py
+++ version2/hangman_functions.py
@@ -3,16 +3,11 @@
 import random
 
 
-def choose_word_from_file(file_path):
-    with open(file_path, "r") as file:
-        words = file.read().splitlines()
-    return random.choice(words)
 
 def display_word(word, guessed_letters):
     # Function to display the word with correctly guessed letters
     for i in word:
         guessed_letters.append(i)
-
         for letter in guessed_letters:
             for v in guessed_letters:
                 if v==letter:
@@ -27,6 +22,3 @@ def display_hangman(wrong_guesses):
     # Function to display the hangman figure based on the number of wrong guesses
     pass
 
-def get_user_guess():
-    # Function to get a valid user guess
-    pass
