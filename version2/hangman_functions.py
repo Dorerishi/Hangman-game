aac6d32894179f1e5cd605dba5048615e3f2a80b
Dorerishi <hrushikeshraghvendra@gmail.com>
2024-02-21 10:31:13 +0530

    learning to use eel

--- version2/hangman_functions.py
+++ version2/hangman_functions.py
@@ -6,18 +6,30 @@ import random
 
 def display_word(word, guessed_letters):
     # Function to display the word with correctly guessed letters
+    word_display=""
     for i in word:
-        guessed_letters.append(i)
-        for letter in guessed_letters:
-            for v in guessed_letters:
-                if v==letter:
-                    word_g+=letter
-                else:
-                    word_g+='_'
-        print(word_g)
-    return guessed_letters
+        if guessed_letters[i]==1:
+            word_display=word_display+i
+        else:
+            word_display=word_display+'_'
+    return word_display)
     pass
 
+def check_letters(word, guessed_letters,gussed_letter,wrongs,message=""):
+    if gussed_letter in word:
+        if guessed_letters[gussed_letter]==1:
+            message="word aldready guessed"
+            word=display_word(word, guessed_letters)
+            return word,message,wrongs
+        else:
+            message="letter in word"
+            word=display_word(word, guessed_letters)
+            return word,message,wrongs
+    else:
+        message="letter is not in word"
+        word=display_word(word, guessed_letters)
+        return word,message,wrongs+1
+    pass
 def display_hangman(wrong_guesses):
     # Function to display the hangman figure based on the number of wrong guesses
     pass
--- version2/nltk_words.py
+++ version2/nltk_words.py
@@ -3,11 +3,11 @@ import random
 import nltk
 from nltk.corpus import words
 
-nltk.download('words')
+#nltk.download('words')
 
 def choose_word_from_nltk():
     english_words = words.words()
     return random.choice(english_words)
 #test
-#x=choose_word_from_nltk()
-#print(x,type(x))
+x=choose_word_from_nltk()
+print(x,type(x))
